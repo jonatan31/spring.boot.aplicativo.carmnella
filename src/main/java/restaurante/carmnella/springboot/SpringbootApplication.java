953da67bdd9267ec157c40b617c954102d73f51b
Jonatan Fabricio Ferreira Batista <jonatan@Laptop-de-Jonatan.local>
2025-03-12 01:06:34 -0300

    first commit

---
+++ src/main/java/restaurante/carmnella/springboot/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package restaurante.carmnella.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/restaurante/carmnella/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package restaurante.carmnella.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
