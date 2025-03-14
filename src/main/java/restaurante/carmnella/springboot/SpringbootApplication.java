261b8f62f6d4374f6e5eae3f7ae553504b043e50
Jonatan Fabricio Ferreira Batista <jonatan@Laptop-de-Jonatan.local>
2025-03-14 01:55:38 -0300

    crinando o controller[aula1]

--- src/main/java/restaurante/carmnella/springboot/SpringbootApplication.java
+++ src/main/java/restaurante/carmnella/springboot/SpringbootApplication.java
@@ -1,8 +1,9 @@
 package restaurante.carmnella.springboot;
 
+import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@Entity
 @SpringBootApplication
 public class SpringbootApplication {
 
